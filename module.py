567075ecf467b85fbd83906c89981b1cff2fcc1d
math-lover31415 <pradyumn3.1415@gmail.com>
2023-05-27 23:34:52 +0530

    ...

--- module.py
+++ module.py
@@ -1,15 +1,16 @@
+import numpy as np
+import pandas as pd
+
+from sklearnex import patch_sklearn
+
+# Patch Scikit Learn
+patch_sklearn()
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+
 def predict(id):
-    import numpy as np
-    import pandas as pd
-
-    from sklearnex import patch_sklearn
-
-    # Patch Scikit Learn
-    patch_sklearn()
-
-    from sklearn.model_selection import train_test_split
-    from sklearn.ensemble import RandomForestRegressor
-    from sklearn.metrics import mean_absolute_error
 
     def f(x):
         try:
@@ -18,32 +19,35 @@ def predict(id):
             return 0
 
     train = pd.read_csv("Datasets/user_books.csv")
-    test=pd.read_csv("Datasets/general_books.csv")
+    test = pd.read_csv("Datasets/general_books.csv")
 
     unwanted_columns = ['title', 'language','isbn','coverImg']
     columns = list(x for x in list(test) if x not in unwanted_columns)
 
-    test = test[columns]
+    test_x = test[columns]
     for column in columns:
-        test[column] = test[column].apply(f)
-    test = test.replace(np.nan, 0)
-    test_x=test.drop('rating', axis=1)
+        test_x[column] = test_x[column].apply(f)
+    test_x = test_x.replace(np.nan, 0)
+    test_x=test_x.drop('rating', axis=1)
 
-    train = train[train['UserId'] == id]
-    train = train[columns]
+    train_X = train[train['UserId'] == id]
+    train_X = train_X[columns]
     for column in columns:
-        train[column] = train[column].apply(f)
-    train = train.replace(np.nan, 0)
+        train_X[column] = train_X[column].apply(f)
+    train_X = train_X.replace(np.nan, 0)
 
-    train_X= train.drop('rating', axis = 1)
-    train_y=train["rating"]
+    train_y = train_X["rating"]
+    train_X = train_X.drop('rating', axis = 1)
 
     model = RandomForestRegressor(random_state=0)
     model.fit(train_X, train_y)
     preds_val = model.predict(test_x)
-    output=test
-    output["rating"]=preds_val
-    print(output.head())
+    output = test
+    output["rating"] = preds_val
+    output = output.merge(train[['title']], on='title', how='left', indicator=True)
+    output = output[output['_merge'] == 'left_only']
+    output = output.sort_values('rating', ascending=False)
+    print(list(output.head()['title']))
 
 if __name__ == '__main__':
     # This code block will only run when the file is executed directly, not when imported
